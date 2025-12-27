0105770122d8f60a1558b60b51e1520c5b220107
Amina <minassouane@gmail.com>
2025-12-27 06:22:09 +0100

    Add basic CSS

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,7 +2,7 @@ import { Card } from "./Card.jsx";
 
 export function Board({ cards }) {
   return (
-    <>
+    <div className="layout">
       {cards.map((card) => (
         <Card
           key={card.id}
@@ -12,6 +12,6 @@ export function Board({ cards }) {
           clicked={card.clicked}
         />
       ))}
-    </>
+    </div>
   );
 }
